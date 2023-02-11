c695f39acb28e65ffe571b0e7e26ef8156c3583c
pokemfeze <maximefeze1@gmail.com>
2023-02-11 01:25:26 +0100

    creation du projet asp

---
+++ Camping_Mediterraneen/Camping_Mediterraneen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Camping_Mediterraneen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
